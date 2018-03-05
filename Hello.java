699f6b476b7a797bb882f77a1371d690793bf7c3
iftekhar <iftekharparvez174@gmail.com>
2018-03-05 15:34:33 +0600

    Added a new file for greeting to heello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Vs code is runny...");
+
+        greeting("md iftkehar");
+    }
+
+    public static void greeting(String name){
+        System.out.println("welcome " + name);
     }
 }

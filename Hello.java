51663221bf93c8b4892f7bd50789ff693cbb751c
iftekhar <iftekharparvez174@gmail.com>
2018-03-05 15:47:28 +0600

    Change greeting method

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Vs code is runny...");
 
-        greeting("md iftkehar");
+        greeting(Git);
     }
 
     public static void greeting(String name){
-        System.out.println("welcome " + name);
+        System.out.println("bye bye " + name);
     }
 }
